856cb31b8393e69ef60f7891d81a5470862e77fb
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-06-08 12:17:53 +0200

    Add docstrings

--- fibonacci.py
+++ fibonacci.py
@@ -3,6 +3,10 @@ from functools import cache
 
 @cache
 def fibonacci(n):
+    '''
+    Calculate the nth fibonacci number using recursion
+    and memoization.
+    '''
     if n == 0:
         return 0
     if n == 1:
@@ -11,6 +15,9 @@ def fibonacci(n):
 
 
 def fib_gen(n):
+    '''
+    A generator yielding the first n fibonacci numbers
+    '''
     for i in range(n):
         yield fibonacci(i)
 

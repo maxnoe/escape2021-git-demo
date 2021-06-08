2cfe18a178ec9d497d77ef66ee962fcba91d654f
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-06-08 09:37:45 +0200

    Add python module to calculate fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+if __name__ == '__main__':
+    print(fibonacci(20))
